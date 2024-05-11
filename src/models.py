0b37221651cdd8ec4941d564574530fa3c2100bb
AlexanderDeMatteo <100955676+AlexanderDeMatteo@users.noreply.github.com>
2024-05-11 03:11:28 +0000

    cambios

--- src/models.py
+++ src/models.py
@@ -1,25 +1,40 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Numeric, Date
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Numeric, Date, Table
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
+association_table = Table(
+    "association_table",
+    Base.metadata,
+    Column('role_id', Integer, ForeignKey('role.id'), primary_key=True),
+    Column('permission_id', Integer, ForeignKey('permission.id'), primary_key=True ),
+)
+
 class User(Base):
     __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    role_id = Column(Integer, ForeignKey('role.id'))
     # user_profile = Column(Integer, ForeignKey('person.id'))
+    name = Column(String(250), nullable=True)
+    last_name = Column(String(250), nullable=True)
+    profile_picture = Column(String(250), nullable=True)
+    dni = Column(Numeric(20), unique=True, nullable=False)
+    gender= Column(String(20), nullable=True)
+    phone_number = Column(Numeric(20), nullable=True)
     email = Column(String(250))
+    motivo_consulta = Column(String(250), nullable=True)
     password = Column(String(250))
     is_psicologo = Column(Boolean(), nullable = True)
     is_active = Column(Boolean(), nullable = True)
     is_online = Column(Boolean(), nullable = True)
-    salt = Column(String(250), unique=True) 
+    salt = Column(String(250), unique=True)
+    role_id = Column(Integer, ForeignKey('role.id'))
+    user_address = Column(Integer, ForeignKey('address.id'))
 
 class Role(Base):
     __tablename__ = 'role'
@@ -38,77 +53,66 @@ class Permission(Base):
     name = Column(String(250))
     description = Column(String(250))
 
+class Address(Base):
+    __tablename__ = "address"
+    id = Column(Integer, primary_key=True)
+    city = Column(String(250), nullable=True)
+    state = Column(String(250), nullable=True)
+
 class Psicology_profile(Base):
     __tablename__ = 'psicology_profile'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    role_id = Column(Integer, ForeignKey('role.id'))
-    name = Column(String(250), nullable=True)
-    last_name = Column(String(250), nullable=True)
-    profile_picture = Column(String(250), nullable=True)
-    dni = Column(Numeric(20), unique=True, nullable=False)
     fpv_number = Column(Numeric(20), unique=True, nullable=False)
-    gender= Column(String(20), nullable=True)
     monto_consulta = Column(Numeric(20), nullable=True)
-    phone_number = Column(Numeric(20), nullable=True)
     specialty_area = Column(String(250))
-    city = Column(String(250), nullable=True)
-    state = Column(String(250), nullable=True)
-    twitter = Column(String(250), nullable=True)
-    facebook = Column(String(250), nullable=True)
-    instagram = Column(String(250), nullable=True)
-    education = Column(String(250), nullable=True)
-    motivo_consulta = Column(String(250), nullable=True)
     psych_strategies = Column(String(250), nullable=True)
     PsychExperiences = Column(String(250), nullable=True)
+    socialNetwork_id = Column(Integer, ForeignKey('socialnetwork.id'))
+    client_list = Column(Integer, ForeignKey('client_list.id'))
 
-class Client_Profile(Base):
-    __tablename__ = 'client_profile'
+
+class SocialNetwork(Base):
+    __tablename__ = 'socialnetwork'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    role_id = Column(Integer, ForeignKey('role.id'))
     name = Column(String(250), nullable=True)
-    last_name = Column(String(250), nullable=True)
-    profile_picture = Column(String(250), nullable=True)
-    dni = Column(Numeric(20), unique=True, nullable=False)
-    gender= Column(String(20), nullable=True)
-    phone_number = Column(Numeric(20), nullable=True)
-    city = Column(String(250), nullable=True)
-    state = Column(String(250), nullable=True)
+    url = Column(String(250), nullable=True)
+    icon = Column(String(250), nullable=True)
+
+class client_List(Base):
+    __tablename__ = "client_list"
+    id = Column(Integer, primary_key=True)   
+    client_id = Column(Integer, ForeignKey('user.id'))
 
 class Client_task(Base):
     __tablename__ = 'client_task'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    role_id = Column(Integer, ForeignKey('role.id'))
-    description = Column(String(250))
-    completed = Column(Boolean(), nullable = True)
+    psicology_task = Column(Integer, ForeignKey('user.id'))
+    client_task = Column(Integer, ForeignKey('user.id'))
 
 class Session(Base):
     __tablename__ = 'session'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    role_id = Column(Integer, ForeignKey('role.id'))
     start_time = Column(String(250), nullable=True)
     end_time = Column(String(250), nullable=True)
     duration_time = Column(String(250), nullable=True)
     reserved = Column(Boolean(), nullable = True)
     calendar_date = Column(Date(), nullable = True)
     room_number = Column(String(250), nullable=True)
+    psicology_host = Column(Integer, ForeignKey('user.id'))
+    client_session = Column(Integer, ForeignKey('user.id'))
 
-class Mi_psicologo(Base):
-    __tablename__ = "mi_psicologo"
-    id = Column(Integer, primary_key=True)   
-    role_id = Column(Integer, ForeignKey('role.id'))
 
 class Payment_acount(Base):
     __tablename__ = "payment_acount"
     id = Column(Integer, primary_key=True)   
-    psiology_profile_id = Column(Integer, ForeignKey(Psicology_profile.id))
     zell_email = Column(String(250), nullable=True)
     binance_route = Column(Numeric(30), nullable=True)
     paypal_user = Column(String(250), nullable=True)
@@ -117,6 +121,7 @@ class Payment_acount(Base):
     pagomovil_bank = Column(Numeric(20), nullable=True)
     pagomovil_ci = Column(Numeric(20), nullable=True)
     pagomovil_phone = Column(Numeric(20), nullable=True)
+    Psicology_profile_id = Column(Integer, ForeignKey('psicology_profile.id'))
 
 class Phrase(Base):
     __tablename__ = "phrase"
